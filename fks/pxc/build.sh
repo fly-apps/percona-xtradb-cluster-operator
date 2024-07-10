875fcc48bc9183fc243987d0c286c750817aa4fb
Joshua Sierles <joshua@fly.io>
2024-07-10 18:37:19 +0200

    bump images

--- fks/pxc/build.sh
+++ fks/pxc/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-IMAGE=flyio/percona-xtradb-cluster-operator:pxc-3
+IMAGE=flyio/percona-xtradb-cluster-operator:pxc-4
 docker build --platform linux/amd64 -t $IMAGE .
 docker push $IMAGE
